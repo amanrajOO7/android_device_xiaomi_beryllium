657051e8165c0fd741360fdd0f0bf5be6a78df1c
Aman Singh <amankumarsingh1412@gmail.com>
2020-04-28 07:42:31 +0530

    beryllium: prebuilts: Add DuckDuckGo, GCam Go & QP Gallery

--- device.mk
+++ device.mk
@@ -73,3 +73,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:system/usr/keylayout/uinput-goodix.kl
+
+# Prebuilt Packages
+PRODUCT_PACKAGES += \
+    DuckDuckGo \
+    GCamGo \
+    QPGallery
---
+++ prebuilts/CleanSpec.mk
@@ -0,0 +1,3 @@
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/DuckDuckGo)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/GCamGo)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/QPGallery)
