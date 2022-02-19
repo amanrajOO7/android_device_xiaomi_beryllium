ccd530fe4483252014e862a1c129284a4f4dd74b
Aman Singh <amankumarsingh1412@gmail.com>
2022-02-19 16:33:35 +0530

    beryllium: Derp-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,4 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_beryllium.mk
+    $(LOCAL_DIR)/derp_beryllium.mk
+
+COMMON_LUNCH_CHOICES := \
+    derp_beryllium-user \
+    derp_beryllium-userdebug \
+    derp_beryllium-eng
